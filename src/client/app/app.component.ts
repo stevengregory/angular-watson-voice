76d0e24b2398cc034e2fa78da44ba487c9b559ea
Steven Gregory <sgregory@gmail.com>
2018-03-05 23:35:41 -0600

    Implemented watson speech to text

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { AppService } from './app.service';
+
+import * as recognizeMicrophone from 'watson-speech/speech-to-text/recognize-microphone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  @Input() text: any;
+  token: any;
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.doVoiceToSpeech();
+  }
+
+  doVoiceToSpeech(): any {
+    this.appService.doVoiceToSpeech().subscribe(token => {
+      this.token = token;
+      console.log('token: ' + this.token);
+      const stream = recognizeMicrophone({
+        token: token,
+        objectMode: true,
+        extractResults: true,
+        format: false
+      });
+      stream.on('data', data => {
+        this.text = data.alternatives[0].transcript;
+        console.log(this.text);
+      });
+    });
+  }
 }
---
+++ src/client/app/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class AppService {
+  private apiUrl = 'http://localhost:3002/api/speech-to-text/token';
+
+  constructor(private http: Http) {}
+
+  doVoiceToSpeech(): Observable<any> {
+    return this.http
+      .get(this.apiUrl)
+      .map(response => response.text())
+      .catch((error: any) => {
+        console.error('Watson API issue', error);
+        return Observable.throw(error.message || error);
+      });
+  }
+}
