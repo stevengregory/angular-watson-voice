88c1aba87b5b23542ba86312657e8b2506449696
Steven Gregory <sgregory@gmail.com>
2018-03-07 10:02:11 -0600

    Removed angular logo & clean-up

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { WatsonComponent } from './watson/watson.component';
 import { WatsonService } from './watson/watson.service';
-
-import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [AppComponent, WatsonComponent],
